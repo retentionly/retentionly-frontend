a804181500118b965b5796e7cc77d133e5eb1fa2
gentleman_byte <25253204+indifferent-omi@users.noreply.github.com>
2022-10-21 10:32:10 +0600

    preview text bug fix

--- src/components/Template/PreviewText/PreviewText.js
+++ src/components/Template/PreviewText/PreviewText.js
@@ -4,12 +4,15 @@ import styled from "@emotion/styled";
 const PreviewTextStyled = styled(Text)`
 // font-weight: 500;
 `
-
+const SlateText = styled.span`
+    &+.slate-text{
+        display:block;
+    }
+`
 const PreviewText = ({ data, tag, placeholder, mainTag }) => {
-    console.log(data);
-    if (data) {
-        const previewText = data[0]?.children[0]?.text || [];
-
+    
+    const previewText = data[0]?.children[0]?.text || [];
+    if (previewText.length > 0) {
         if (data?.length < 1 && !tag) {
             return <>{previewText}</>
         } else if (data.length < 1) {
@@ -17,12 +20,12 @@ const PreviewText = ({ data, tag, placeholder, mainTag }) => {
         } else {
             return (<>
                 {data.map((item, key) => {
-                    return (<span className="slate-text" key={key}>{item.children.map(({ text, bold, underline, italic }) => {
+                    return (<SlateText className="slate-text" key={key}>{item.children.map(({ text, bold, underline, italic }) => {
 
                         return <PreviewTextStyled color="#000" key={key + 'fjd'} as="span" fontWeight={bold && "bold"} textDecor={underline && "underline"} style={{ fontStyle: `${italic ? 'italic' : 'normal'}`, display: 'inline', fontFamily: "inherit" }}>
-                            {" "}{text ? text : placeholder}
+                            {" "}{text && text}
                         </PreviewTextStyled>
-                    })}</span>)
+                    })}</SlateText>)
                 })}
             </>)
         }
