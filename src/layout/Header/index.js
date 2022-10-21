d47d3fc01c61f333cd5b633fb09982dee39355da
gentleman_byte <25253204+indifferent-omi@users.noreply.github.com>
2022-10-21 14:23:44 +0600

    pricing removed

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -30,7 +30,7 @@ export default function Header() {
             <HeaderContent>
                 <Menu>
                     {pathname !== "/register" && <Menu.Item className={!user && 'disabled'}><Link to="/email" >Dashboard</Link></Menu.Item>}
-                    {pathname !== "/login" && <Menu.Item className={!user && 'disabled'}><Link to="/">Pricing</Link></Menu.Item>}
+                    {/* {pathname !== "/login" && <Menu.Item className={!user && 'disabled'}><Link to="/">Pricing</Link></Menu.Item>} */}
                     {pathname !== "/register" && <Menu.Item className={!user && 'disabled'}><Link to="/master">Master</Link></Menu.Item>}
                     <Menu.Item className={!user && 'disabled'}><Link to="/">Home</Link></Menu.Item>
                 </Menu>
