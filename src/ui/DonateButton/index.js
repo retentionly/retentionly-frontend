49c675ec21de3e69be15f099ba8156f5314ac198
gentleman_byte <25253204+indifferent-omi@users.noreply.github.com>
2022-10-21 14:19:16 +0600

    donate button fixed

--- src/ui/DonateButton/index.js
+++ src/ui/DonateButton/index.js
@@ -2,19 +2,35 @@ import { Button } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 import React from 'react'
 
-const DonateButtonStyled = styled(Button)(({ link }) => (`
-background: #F6C5C5;
-color: #000000;
+const DonateButtonStyled = styled("span")(({ link,disabled }) => (`
+background: #000000;
+color: #fff;
 border-radius: 0;
-padding: 30px 67px;
+height:50px;
+display: inline-flex;
+align-items: center;
+justify-content: center;
 font-size: 20px;
 font-weight: 500;
 margin: 20px 0;
+
+transition:.4s;
+cursor: ${disabled ? "not-allowed" : "pointer"};
 &:hover{
-    background: #F6D5D9;
+    opacity:.8
 }
 a {
     pointer-events: ${link ? "all" : "none"};
+    height:100%;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    padding-top:10px;
+    padding-bottom:10px;
+    padding-left:25px;
+    padding-right:25px;
+    justify-content: center;
 }
 `))
 
@@ -22,6 +38,7 @@ const DonateButton = ({ url }) => {
 
     const urlText = url[0]?.children[0]?.text;
     const link = urlText && (urlText?.includes("https://") ? urlText : `https://${urlText}`)
+    // const link = "https://facebook.com";
 
     return (
         <DonateButtonStyled disabled={!link} link={link}>
