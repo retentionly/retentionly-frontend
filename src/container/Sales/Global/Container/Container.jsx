831301d69e8068409696823a9c80aaf5c7fe38a2
Robiul Hasan <hello@retentionly.co.uk>
2022-10-29 18:23:38 +0600

    Container created

---
+++ src/container/Sales/Global/Container/Container.jsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled'
+import React from 'react'
+
+const ContainerStyled = styled.div`
+width: 100%;
+margin: 0 auto;
+
+@media (min-width: 576px) {
+    max-width: 540px;
+}
+@media (min-width: 768px) {
+    max-width: 720px;
+}
+@media (min-width: 992px) {
+    max-width: 960px;
+}
+@media (min-width: 1200px) {
+    max-width: 1140px;
+}
+@media (min-width: 1400px) {
+    max-width: 1320px;
+}
+`
+
+const Container = ({ children }) => {
+  return (
+    <ContainerStyled>
+        {children}
+    </ContainerStyled>
+  )
+}
+
+export default Container
---
+++ src/container/Sales/Stats/Stats.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Container from '../Global/Container'
+
+const Stats = () => {
+  return (
+    <Container>Stats</Container>
+  )
+}
+
+export default Stats
